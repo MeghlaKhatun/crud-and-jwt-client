99ab1f8500f9ad1d902a25d631f837427a312477
meghla khatun <meghlaparvin45@gmail.com>
2023-11-07 11:46:56 +0600

    added a modal in the single service page

--- src/Pages/SingleService/SingleService.jsx
+++ src/Pages/SingleService/SingleService.jsx
@@ -1,10 +1,14 @@
 import { useLoaderData } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
+import { useContext } from "react";
+import { AuthContext } from "../../Routes/Provider/AuthProvider";
 
 const SingleService = () => {
 
+    const { user } = useContext(AuthContext)
+
     const singeService = useLoaderData();
-    const { image, service_name, profile, name, price, description } = singeService;
+    const { image, service_name, email, profile, name, price, description } = singeService;
 
 
     return (
@@ -16,7 +20,7 @@ const SingleService = () => {
                         <img className='md:rounded-l-lg h-full w-full' src={image} alt="" />
                     </div>
                     <div className='flex items-center flex-1'>
-                        <div className="card-body ">                          
+                        <div className="card-body ">
                             <div className='flex gap-3 items-center'>
                                 <div>
                                     <img src={profile} className="w-[30px] h-[30px] rounded-full" alt="" />
@@ -29,9 +33,63 @@ const SingleService = () => {
                             <h2 className="text-black font-bold text-xl md:text-2xl italic">Service: {service_name}</h2>
                             <h2 className="text-black font-medium text-[18px] md:text-xl italic">Price: ${price}</h2>
                             <h2 className="text-black font-medium italic"> {description}</h2>
+
+
+                            {/* modal section */}
+
+                            {/* You can open the modal using document.getElementById('ID').showModal() method */}
+
                             <div>
-                                <button className="bg-[#009866] border-none italic text-white py-2 md:py-3 px-4 md:px-5 font-semibold rounded-lg">Book Now</button>
+                                <button onClick={() => document.getElementById('my_modal_3').showModal()} className="bg-[#009866] border-none italic text-white py-2 md:py-3 px-4 md:px-5 font-semibold rounded-lg">Book Now</button>
                             </div>
+
+                            <dialog id="my_modal_3" className="modal">
+                                <div className="modal-box">
+                                    <form method="dialog">
+                                        {/* if there is a button in form, it will close the modal */}
+                                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                                    </form>
+
+                                    {/* main form */}
+                                    <div>
+                                        <img src={image} alt="" />
+                                    </div>
+
+                                    <form>
+
+                                        <div className="form-control pt-2 flex-1">
+                                            <input type="text" name="serviceName" defaultValue={service_name} readOnly className="input font-bold input-bordered text-gray-600" required />
+                                        </div>
+
+                                        <div className="form-control pt-2 flex-1">
+                                            <input type="text" name="proEmail" defaultValue={email} readOnly className="input font-bold input-bordered text-gray-600" required />
+                                        </div>
+
+                                        <div className="form-control pt-2 flex-1">
+                                            <input type="text" name="user" defaultValue={user?.email} readOnly className="input font-bold input-bordered text-gray-600" required />
+                                        </div>
+
+                                        <div className="form-control pt-2 flex-1">
+                                            <input type="text" name="price" defaultValue={price} className="input font-bold input-bordered text-gray-600" required />
+                                        </div>
+
+                                        <div className="form-control pt-2 flex-1">
+                                            <input type="date" name="date" className="input input-bordered text-black" required />
+                                        </div>
+
+                                        <div className="form-control pt-2 flex-1">
+                                            <textarea name="description" id="" cols="10" rows="3" placeholder="Special Instruction" className="border-2 rounded-lg outline-none text-black"></textarea>
+                                        </div>
+
+                                    </form>
+
+                                    <div className="flex items-center justify-center mt-4">
+                                        <button className="bg-[#009866] border-none italic text-white py-2 md:py-3 px-4 md:px-5 font-semibold rounded-lg">Purchase</button>
+                                    </div>
+
+                                </div>
+                            </dialog>
+
 
 
 
@@ -40,7 +98,7 @@ const SingleService = () => {
                 </div>
 
             </div>
-        </div>
+        </div >
     );
 };
 
